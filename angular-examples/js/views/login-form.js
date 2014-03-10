a87a0faaaea7621ba16debc6e5a2f9eaf1cdb2dc
Vikash Sharma <vikash.sharma@zeuslearning.com>
2014-03-10 19:48:07 +0530

    Add password validation

--- angular-examples/js/views/login-form.js
+++ angular-examples/js/views/login-form.js
@@ -1,24 +1,24 @@
-﻿(function () {
-    LoginView = Backbone.View.extend({
-        el: '#leave-tracker-login-form',
-        events: {
-            'blur .login-password': '_validatePassword'
-        },
+﻿
+LoginView = Backbone.View.extend({
+    el: '#leave-tracker-login-form',
+    events: {
+        'blur .login-password': '_validatePassword'
+    },
 
-        intialize: function () {
-        },
-        render: function () {
-        },
-        _validatePassword: function (event) {
-            var $element = $(event.target),
-                strPassword = $element.val(),
-                regex = /(?=^.{8,32}$)(?=(?:.*?\d){1})(?=.*[a-z])(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%*()_+^&]*$/;
-                if (regex.test(strPassword)) {
-                $element.addClass('invalid');
-            }
-            else {
-                $element.removeClass('invalid');
-            }
+    intialize: function () {
+    },
+    render: function () {
+    },
+    _validatePassword: function (event) {
+        var $element = $(event.target),
+            strPassword = $element.val(),
+            regex = /(?=^.{8,32}$)(?=(?:.*?\d){1})(?=.*[a-z])(?=(?:.*?[!@#$%*()_+^&}{:;?.]){1})(?!.*\s)[0-9a-zA-Z!@#$%*()_+^&]*$/;
+        if (regex.test(strPassword)) {
+            $element.removeClass('invalid');
         }
-    });
-})();
+        else {
+            $element.addClass('invalid');
+            
+        }
+    }
+});
